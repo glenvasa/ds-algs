a9f93e5153d5509e96057b52dbabb7490144b423
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-10-15 10:27:56 -0400

    Update array-min-number.js

--- array-min-number.js
+++ array-min-number.js
@@ -6,9 +6,18 @@
 
 // Time Complexity is O(n) b/c operations increase linearly with increase in n
 
+// Best Case: [1,2,3] => n
+// Worst Case: [3,2,1] => n
+// Average Case: [2, 3, 1] => n
+
 const minValue = (arr) => {
-    arr.sort((a, b) => a-b)
-    return arr[0]
+    let min = arr[0]
+    for(const num of arr){ // n
+       num < min ? min = num : null // 0 (best case), 2 (worst case), 1 (avg case) 
+    }
+    return min
+    // arr.sort((a, b) => a-b)
+    // return arr[0]
 }
 
 console.log(minValue([7, 9, 4, 44, 13]))
@@ -25,7 +34,7 @@ console.log(minValue([17, 91, 433, 14, 113]))
 // Time complexity is O(1) because constant # of operations (1) no matter what the number is.
 
 const isEven = (num) => {
-    return num % 2 === 0 ? true : false
+    return num % 2 === 0 
 }
 
 console.log(isEven(13));
