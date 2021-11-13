1e2cb129c832ac2abb71ea07c3da1924d7c648cf
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-11-13 11:20:37 -0500

    Update dynamic.js

--- dynamic.js
+++ dynamic.js
@@ -1,8 +1,23 @@
-const memo = {}
 
-function fib(n) {
-    if (n === 0 || n === 1) {
-        return 1
+
+
+function fib(n, memo) {
+    let result
+    console.log(memo);
+    if (memo[n]) {
+        return memo[n]
     }
-    return fib(n-1) + fib(n-2)
+    if (n === 0 || n === 1) {
+        result = 1
+    } else {
+        result = fib(n-1, memo) + fib(n-2, memo)
+    }
+    
+    memo[n] = result
+    return result
 }
+
+// fib seq [1,1,2,3,5,8,13,21]
+
+//console.log(fib(4, {})); // 5
+console.log(fib(7, {})); // 21
